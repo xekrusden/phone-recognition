33d881b4e6dbe8625754110dce482a4bf42d4627
Anton Denisenko <xekrusden@ya.ru>
2017-06-01 13:13:44 +0300

    Update index.php

--- examples/index.php
+++ examples/index.php
@@ -1,13 +1,15 @@
-<?php
+    <?php
+
 require __DIR__ . '/../vendor/autoload.php';
 
-use \aotd\Captcha\AvitoPhone;
+    use \aotd\Captcha\AvitoPhone;
 
 /* @var DirectoryIterator $file */
 foreach (new DirectoryIterator(__DIR__ . '/test') as $file) {
     if ($file->getExtension() !== 'png') {
         continue;
-    }
+       
+        }
 
     $image = new Imagick($file->getPathname());
     $checkValue = $file->getBasename('.png');
@@ -19,5 +21,6 @@ foreach (new DirectoryIterator(__DIR__ . '/test') as $file) {
         echo "Recognition error. Recognized value '$recognized' differs original value '$checkValue'\n";
     } else {
         echo "Recognition $recognized - ok!\n";
-    }
+        
+     }
 }
